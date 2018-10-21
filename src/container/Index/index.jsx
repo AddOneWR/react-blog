a8bce693dd848f87f3d11989283a9ce8a5174d33
AddOndDn <467072280@qq.com>
2018-10-21 23:19:56 +0800

    添加文章: RxJs是个不错的东西 以及 删除多余目录

--- src/container/Index/index.jsx
+++ src/container/Index/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Banner from '../../component/Banner/Banner';
 import Footer from '../../component/Footer/Footer';
 
-import '../../style/index/index.scss';
+import './index.scss';
 
 class Index extends Component {
   render() {
