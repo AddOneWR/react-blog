a7d316c0c4734590e0beb5f4e4aa9fdc5c0fa676
AddOndDn <467072280@qq.com>
2018-11-02 16:08:56 +0800

    小工具: 正规式自动机;gif制作 添加

--- src/container/AboutMe/AboutMe.jsx
+++ src/container/AboutMe/AboutMe.jsx
@@ -9,9 +9,12 @@ const AboutMe = () => (
     <p>前端开发者，Javascript爱好者</p>
     <p>微信：eDQ2NzA3MjI4MA==</p>
     <p>曾实习: 九章算法，杭州有赞</p>
+    <p>小工具 ↓</p>
     <ul>
       <li>
         <a href="http://www.addoneg.com/js2c/index.html" target="_blank"  rel="noopener noreferrer nofollow">C语言转JS超微型编译器</a>
+        <a href="http://www.addoneg.com/exp2nfa/index.html" target="_blank"  rel="noopener noreferrer nofollow">正规式 ←→ NFA</a>
+        <a href="http://www.addoneg.com/gifmake/index.html" target="_blank"  rel="noopener noreferrer nofollow">在线GIF图制作~</a>
       </li>
     </ul>
   </div>
