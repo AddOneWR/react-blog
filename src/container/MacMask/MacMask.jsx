81fd11ae2f27aff3a1da7c3f6fcabd4807a7867a
AddOndDn <467072280@qq.com>
2018-11-05 20:32:57 +0800

    背景图片压缩优化

--- src/container/MacMask/MacMask.jsx
+++ src/container/MacMask/MacMask.jsx
@@ -5,7 +5,7 @@ import './mask.scss';
 const MacMask = () => (
   <div className="mask-container">
     <div className="mask-main">
-      <img src="http://p5sf6v0wz.bkt.clouddn.com/avatar.jpg" className="mask-avatar" alt="avatar"/>
+      <img src="http://p5sf6v0wz.bkt.clouddn.com/avatar_gaitubao_com_80x80.jpg" className="mask-avatar" alt="avatar" width="80" height="80"/>
       <p>徐嘉一(AddOneG)</p>
       <p>南京航空航天大学软件工程专业在读 20届毕业</p>
       <p>前端开发者，Javascript爱好者</p>
