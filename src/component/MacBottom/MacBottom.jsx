72aee50c4b17f4e971d5259695060b71ce4af5be
AddOndDn <467072280@qq.com>
2018-12-13 15:36:16 +0800

    底部nav hover样式添加

--- src/component/MacBottom/MacBottom.jsx
+++ src/component/MacBottom/MacBottom.jsx
@@ -4,16 +4,18 @@ import './macbottom.scss';
 
 const MacBottom = (props) => (
   <div className="mac-bottom-container">
-    <a href="https://github.com/AddOneDn" target="_blank" rel="noopener noreferrer nofollow">
-      <img src='https://s1.ax1x.com/2018/11/21/F9hMCR.png' className="icon" alt="icon"></img>
-    </a>
-    <a href="https://www.npmjs.com/~xujiayihaoren1" target="_blank" rel="noopener noreferrer nofollow">
-      <img src='https://s1.ax1x.com/2018/11/21/F9hmE4.png' className="icon" alt="icon"></img>    
-    </a>
-    <a href="https://juejin.im/user/5800c5b267f3560058a9f89f" target="_blank" rel="noopener noreferrer nofollow">
-      <img src='https://s1.ax1x.com/2018/11/21/F9hZbF.png' className="icon" alt="icon"></img>    
-    </a>
-    <img src='https://s1.ax1x.com/2018/11/21/F9hlgx.png' className="icon" alt="icon" onClick={props.handleSearch}></img>
+    <div className="mac-bottom-main">
+      <a href="https://github.com/AddOneDn" target="_blank" rel="noopener noreferrer nofollow">
+        <img src='https://s1.ax1x.com/2018/11/21/F9hMCR.png' className="icon" alt="icon"></img>
+      </a>
+      <a href="https://www.npmjs.com/~xujiayihaoren1" target="_blank" rel="noopener noreferrer nofollow">
+        <img src='https://s1.ax1x.com/2018/11/21/F9hmE4.png' className="icon" alt="icon"></img>    
+      </a>
+      <a href="https://juejin.im/user/5800c5b267f3560058a9f89f" target="_blank" rel="noopener noreferrer nofollow">
+        <img src='https://s1.ax1x.com/2018/11/21/F9hZbF.png' className="icon" alt="icon"></img>    
+      </a>
+      <img src='https://s1.ax1x.com/2018/11/21/F9hlgx.png' className="icon" alt="icon" onClick={props.handleSearch}></img>
+    </div>
   </div>
 )
 
