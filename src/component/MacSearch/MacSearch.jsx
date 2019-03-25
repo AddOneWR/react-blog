e71390a1934842e9462b703bb7d6d18f78a36a0b
AddOndDn <467072280@qq.com>
2019-03-25 11:22:04 +0800

    fix: macSearch react hooks update

--- src/component/MacSearch/MacSearch.jsx
+++ src/component/MacSearch/MacSearch.jsx
@@ -1,53 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import './search.scss';
 
-class MacSearch extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: ''
-    }
-  }
-  
-  handleChange = (e) => {
-    this.setState({
-      value: e.target.value
-    })
-  }
+const MacSearch = ({ onSearch, onClose }) => {
+  const [value, setValue] = useState('');
 
-  onKeyUp = (e) => {
-    if(e.keyCode === 13) {
-      let value = this.state.value;
-      this.setState({
-        value: ''
-      })
-      this.props.onSearch(value)
-    }
-  }
-
-  onBlur = () => {
-    this.props.onClose();
-  }
-
-  render() {
-    const { value } = this.state;
-
-    return (
-      <div className="macsearch-container">
-        <img src="https://s1.ax1x.com/2018/11/21/F9hlgx.png" alt="search" className="macsearch-icon"></img>
-        <input 
-          className="macsearch-input" 
-          autoFocus="autofocus" 
-          onChange={this.handleChange}
-          value={value}
-          onKeyUp={this.onKeyUp}
-          onBlur={this.onBlur}
-        >
-        </input>
-      </div>
-    )
-  }
+  return (
+    <div className="macsearch-container">
+      <img src="https://s1.ax1x.com/2018/11/21/F9hlgx.png" alt="search" className="macsearch-icon"></img>
+      <input 
+        className="macsearch-input" 
+        autoFocus="autofocus" 
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+        onKeyUp={(e) => {
+          if(e.keyCode === 13) {
+            setValue('');
+            onSearch(value)
+          }
+        }}
+        onBlur={onClose}
+      >
+      </input>
+    </div>
+  )
 }
 
 export default MacSearch;
